63b7b34b9ac09872182551f5e826e713a03cea9a
Brian Hardy <brian@bignerdranch.com>
2014-10-08 22:01:20 -0400

    Implements remaining TCPAcceptSocket tests

--- DeferredTCPSocket/DeferredTCPSocketMacTests/TCPAcceptSocketTests.swift
+++ DeferredTCPSocket/DeferredTCPSocketMacTests/TCPAcceptSocketTests.swift
@@ -10,6 +10,7 @@ import Cocoa
 import XCTest
 import DeferredTCPSocketMac
 import ResultMac
+import DeferredMac
 
 class TCPAcceptSocketTests: XCTestCase {
 
@@ -36,16 +37,62 @@ class TCPAcceptSocketTests: XCTestCase {
     }
     
     func testAccept_portRequiringRoot() {
-        XCTFail("not implemented")
+        let connHandler = TCPAcceptSocket.ConnectionHandler(queue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), callback: { commSocket in
+            // do-nothing
+        })
+        
+        let result = TCPAcceptSocket.accept(onPort: 1023, withConnectionHandler: connHandler)
+        XCTAssertNil(result.successValue, "Should NOT have a success value")
+        XCTAssertTrue(result.failureValue is LibCError, "Should have a failure value")
+        if let error = result.failureValue as? LibCError {
+            XCTAssertEqual(error.functionName, "bind", "Should fail on bind")
+            XCTAssertEqual(error.errno, EACCES, "Should fail with EACCES")
+        }
     }
     
     func testAccept_portZero() {
-        XCTFail("not implemented")
+        let connHandler = TCPAcceptSocket.ConnectionHandler(queue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), callback: { commSocket in
+            // do-nothing
+        })
+        
+        let result = TCPAcceptSocket.accept(onPort: 0, withConnectionHandler: connHandler)
+        XCTAssertNotNil(result.successValue, "Should have a success value")
+        // TODO: This works, but it should give us some way to know what port it used; man getsockname
+        if let acceptSocket = result.successValue {
+            acceptSocket.close()
+        }
     }
     
     func testConnection() {
-        XCTFail("not implemented")
+        
+        let connHandler = TCPAcceptSocket.ConnectionHandler(queue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), callback: { commSocket in
+            // force this to block waiting for the write to finish by calling `value`
+            commSocket.writeString("hello").value
+            return
+        })
+        
+        let result = TCPAcceptSocket.accept(onPort: 12345, withConnectionHandler: connHandler)
+        XCTAssertNotNil(result.successValue, "Should have a success value")
+        if let acceptSocket = result.successValue {
+            
+            // connect to port 12345 using a TCPCommSocket
+            let defferedSocketResult = TCPCommSocket.connectToHost("localhost", serviceOrPort: "12345")
+            // block waiting for value
+            let socketResult = defferedSocketResult.value
+            XCTAssertNotNil(socketResult.successValue, "Should have a successful connection")
+            if let socket = socketResult.successValue {
+                let deferredReadResult = socket.readDataToLength(5)
+                // block waiting for value
+                let readResult = deferredReadResult.value
+                XCTAssertNotNil(readResult.successValue, "Should have a successful data result")
+                if let data = readResult.successValue {
+                    let readString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+                    XCTAssertEqual(readString, "hello", "Should receive 'hello'")
+                }
+            }
+            
+            acceptSocket.close()
+        }
     }
-    
 
 }
